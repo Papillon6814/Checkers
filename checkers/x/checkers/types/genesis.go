8e7d069ca7828ec78db69e1fc5487334ecbbf037
Papillon6814 <so1rou.k@gmail.com>
2022-04-27 23:06:38 +0900

    feat: createGame message

--- checkers/x/checkers/types/genesis.go
+++ checkers/x/checkers/types/genesis.go
@@ -10,9 +10,9 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-        StoredGameList: []*StoredGame{},
-        NextGame:       &NextGame{"", uint64(0)},
-    }
+		StoredGameList: []*StoredGame{},
+		NextGame:       &NextGame{"", uint64(0)},
+	}
 }
 
 // Validate performs basic genesis state validation returning an error upon any
